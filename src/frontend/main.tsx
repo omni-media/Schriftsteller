6e42dbd2fd056e2846fb6cb592c768457bbf6c74
zenkyu <przemekgg2002@gmail.com>
2023-10-17 23:15:00 +0200

    add: index html template for ssg

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {HashRouter} from 'react-router-dom'
 
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
