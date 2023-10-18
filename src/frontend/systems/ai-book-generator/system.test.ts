fcb68d908468b097ae13f30280a6ded7b14d8d1c
zenkyu <przemekgg2002@gmail.com>
2023-10-18 21:49:40 +0200

    add: ai book generator tests

---
+++ src/frontend/systems/ai-book-generator/system.test.ts
@@ -0,0 +1,13 @@
+import {AIBookGenerator} from "./system"
+
+describe("generating-ai-book", () => {
+	const generator = new AIBookGenerator()
+	console.log(generator)
+	it("should generate book that is between 5000-25000 words long", () => {})
+	it("should generate book that is different than other ones", () => {})
+	it("should generate 10 books per day, each of different genre", () => {})
+	it("should handle diverse input genres and generate relevant books", () => {})
+	it("should generate a book with valid chapters", () => {})
+	it("should generate a book with valid metadata", () => {})
+	it("should generate books with appropriate language and grammar", () => {})
+})
