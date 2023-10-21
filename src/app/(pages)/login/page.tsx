4248337a350d2b2fc1b2fdecc6c3d26819f065e4
zenkyu <przemekgg2002@gmail.com>
2023-10-21 19:21:17 +0200

    add: react query provider

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+export default function Login() {
+	return (
+		<form>
+			<label>Username</label>
+			<input />
+			<label>Password</label>
+			<input />
+			<a href="/ai-books">login</a>
+		</form>
+	)
+}
