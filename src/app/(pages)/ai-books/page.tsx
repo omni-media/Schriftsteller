5600752dc377628d405e3c293445a75ae496e7d2
zenkyu <przemekgg2002@gmail.com>
2023-10-23 23:57:58 +0200

    add: basic styles for books list page

--- src/app/(pages)/ai-books/page.tsx
+++ src/app/(pages)/ai-books/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import Link from "next/link"
-import {Book } from "@/app/types"
+import Image from "next/image"
+import {Book} from "@/app/types"
+import styles from "./style.module.css"
 import {get_books} from "@/app/api/get_books"
 import {useQuery} from "@tanstack/react-query"
 import Navigation from "@/app/components/navigation/component"
@@ -15,15 +17,20 @@ export default function AIBooks() {
 
 	return (
 		<>
+			<h1 className={styles.mainHeading}>Schriftsteller</h1>
 			<Navigation />
-			<div>AiBooks</div>
-			{data?.map((book, i) => (
-				<div key={i}>
-					<h2>{book.title}</h2>
-					<p>{book.description}</p>
-					<Link href={"ai-books" + `/${book.title.split(" ").join("-")}`}>read book</Link>
-				</div>
-			))}
+			<h2 className={styles.routeHeading}>AIBOOKS</h2>
+			<div className={styles.books}>
+				{data?.map((book, i) => (
+					<div className={styles.book} key={i}>
+						<h2>{book.title}</h2>
+						<Image alt="book cover" width={200} height={250} src="/book-cover.png"/>
+						<Link href={`ai-books/${book.title.split(" ").join("-")}`}>
+							read book
+						</Link>
+					</div>
+				))}
+			</div>
 		</>
 	)
 }
