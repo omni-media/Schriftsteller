e526853abfea3e9aaad0cbac30609507c3989a53
zenkyu <przemekgg2002@gmail.com>
2023-10-24 16:38:01 +0200

    improve: navigation, ai-books pages styles

--- src/app/components/navigation/component.tsx
+++ src/app/components/navigation/component.tsx
@@ -1,31 +1,34 @@
 "use client"
 import Link from "next/link"
+import Image from "next/image"
 import styles from "./style.module.css"
 import {usePathname} from "next/navigation"
 
 export default function Navigation() {
 	const location = usePathname()
-
 	return (
-		<nav className={styles.navigation}>
-			<ol>
-				<li>
-					<Link className={location === "/ai-books" ? styles.active : ""} href="/ai-books">
-						AI-books
-					</Link>
-				</li>
-				<li>
-					<Link className={location === "/game" ? styles.active : ""} href="/game">
-						Game
-					</Link>
-				</li>
-				<li>
-					<Link className={location === "/profile" ? styles.active : ""} href="/profile">
-						Profile
-					</Link>
-				</li>
-				<li><Link className={styles.logout} href="/">Logout</Link></li>
-			</ol>
-		</nav>
+		<>
+			<Image className={styles.logo} alt="logo" width={900} height={150} src="/schriftsteller-logo.png" />
+			<nav className={styles.navigation}>
+				<ol>
+					<li>
+						<Link className={location === "/ai-books" ? styles.active : ""} href="/ai-books">
+							AI-books
+						</Link>
+					</li>
+					<li>
+						<Link className={location === "/game" ? styles.active : ""} href="/game">
+							Game
+						</Link>
+					</li>
+					<li>
+						<Link className={location === "/profile" ? styles.active : ""} href="/profile">
+							Profile
+						</Link>
+					</li>
+					<li><Link className={styles.logout} href="/">Logout</Link></li>
+				</ol>
+			</nav>
+		</>
 	)
 }
