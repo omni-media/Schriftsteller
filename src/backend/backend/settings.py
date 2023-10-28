f5635d00069f84ce0eb0227974a7e701c1da09fe
Jakub Kubiś <jakubkubis45@gmail.com>
2023-10-28 12:01:36 +0200

    add: celery

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -140,3 +140,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
---
+++ src/backend/book/tasks.py
@@ -0,0 +1,4 @@
+from .scripts.make_book import make_book
+import os
+from django.conf import settings
+
