285cf9f44e6bd30a7b20478a27df5ecde4913931
Jakub Kubiś <jakubkubis45@gmail.com>
2023-10-28 15:32:06 +0200

    add: docker

--- src/backend/book/models.py
+++ src/backend/book/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class BookModel(models.Model):
-    filePath = models.FilePathField(max_length=300)
+    filePath = models.FileField(max_length=300,default='/book')
 
     def __str__(self):
         return "Book(" + str(self.pk) + ")"
--- src/backend/book/tasks.py
+++ src/backend/book/tasks.py
@@ -1,4 +1,10 @@
 from .scripts.make_book import make_book
 import os
 from django.conf import settings
+from pathlib import Path
+from django.core.files import File
+
+
+
+
 
