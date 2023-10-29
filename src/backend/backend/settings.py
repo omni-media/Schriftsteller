2573a299344c428b83d813f2233b11b73ff76d5d
Jakub Kubiś <jakubkubis45@gmail.com>
2023-10-29 20:50:51 +0100

    update: requirements.txt

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -142,5 +142,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
--- src/backend/book/models.py
+++ src/backend/book/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-class BookModel(models.Model):
-    filePath = models.FileField(max_length=300,default='/book')
+class Book(models.Model):
+    filepath = models.FileField(max_length=300, default='/book')
 
     def __str__(self):
         return "Book(" + str(self.pk) + ")"
